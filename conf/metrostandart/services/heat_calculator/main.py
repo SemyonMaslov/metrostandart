f1ac14f3d61a721dcf7fd69aebb22891bd81ebb7
aleksandr razarenov <aleksandr.razarenov@saltpepper.ru>
2023-09-10 18:51:27 +0300

    added all heat_calculator

--- conf/metrostandart/services/heat_calculator/main.py
+++ conf/metrostandart/services/heat_calculator/main.py
@@ -1,122 +1,77 @@
+from PyPDF2 import PdfReader
 from django import forms
 from service_objects.services import Service
 
-from metrostandart.services.heat_meter.completion_excel import (
-    CompletionHeatMeterService
+from metrostandart.services.heat_calculator.completion_excel_CTD import (
+    CompletionHeatCalculatorCTDService
 )
-from metrostandart.services.heat_meter.completion_excel_2811204 import (
-    CompletionHeatMeter2811204Service
+from metrostandart.services.heat_calculator.completion_excel_TB7 import (
+    CompletionHeatCalculatorTB7Service
 )
-from metrostandart.services.heat_meter.completion_excel_2811209 import (
-    CompletionHeatMeter2811209Service
+from metrostandart.services.heat_calculator.completion_excel_TCPB023 import (
+    CompletionHeatCalculatorTCPB023Service
 )
-from metrostandart.services.heat_meter.completion_excel_2811214 import (
-    CompletionHeatMeter2811214Service
+from metrostandart.services.heat_calculator.completion_excel_TCPB024 import (
+    CompletionHeatCalculatorTCPB024Service
 )
-from metrostandart.services.heat_meter.completion_excel_3872308 import (
-    CompletionHeatMeter3872308Service
+from metrostandart.services.heat_calculator.completion_excel_TCPB024M import (
+    CompletionHeatCalculatorTCPB024MService
 )
-from metrostandart.services.heat_meter.completion_excel_4502410 import (
-    CompletionHeatMeter45024410Service
+from metrostandart.services.heat_calculator.completion_excel_TCPB026M import (
+    CompletionHeatCalculatorTCPB026MService
 )
-from metrostandart.services.heat_meter.completion_excel_4931812 import (
-    CompletionHeatMeter4931812Service
+from metrostandart.services.heat_calculator.completion_excel_TCPB033 import (
+    CompletionHeatCalculatorTCPB033Service
 )
-from metrostandart.services.heat_meter.completion_excel_5544013 import (
-    CompletionHeatMeter5544013Service
+from metrostandart.services.heat_calculator.completion_excel_TCPB034 import (
+    CompletionHeatCalculatorTCPB034Service
 )
-from metrostandart.services.heat_meter.completion_excel_5545713 import (
-    CompletionHeatMeter5545713Service
+from metrostandart.services.heat_calculator.completion_excel_TEM104 import (
+    CompletionHeatCalculatorTEM104Service
 )
-from metrostandart.services.heat_meter.completion_excel_5800314 import (
-    CompletionHeatMeter5800314Service
+from metrostandart.services.heat_calculator.completion_excel_TEM106 import \
+    CompletionHeatCalculatorTEM106Service
+from metrostandart.services.heat_calculator.completion_excel_VKT5 import (
+    CompletionHeatCalculatorVKT5Service
 )
-from metrostandart.services.heat_meter.completion_excel_6309316 import (
-    CompletionHeatMeter6309316Service
+from metrostandart.services.heat_calculator.completion_excel_VKT7 import (
+    CompletionHeatCalculatorVKT7Service
 )
-from metrostandart.services.heat_meter.completion_excel_6549816 import (
-    CompletionHeatMeter6549816Service
-)
-from metrostandart.services.heat_meter.completion_excel_7181218 import (
-    CompletionHeatMeter7181218Service
-)
+from metrostandart.utils import collection_data
+
+SERVICES_DICT = {
+    'ВКТ-7': CompletionHeatCalculatorVKT7Service,
+    'ВКТ': CompletionHeatCalculatorVKT5Service,
+    'СТД': CompletionHeatCalculatorCTDService,
+    'ТВ7': CompletionHeatCalculatorTB7Service,
+    'ТСРВ-023': CompletionHeatCalculatorTCPB023Service,
+    'ТСРВ-033': CompletionHeatCalculatorTCPB033Service,
+    'ТСРВ-034': CompletionHeatCalculatorTCPB034Service,
+    'ТСРВ-024': CompletionHeatCalculatorTCPB024Service,
+    'ТСРВ-024м': CompletionHeatCalculatorTCPB024MService,
+    'ТСРВ-026м': CompletionHeatCalculatorTCPB026MService,
+    'ТЭМ-104': CompletionHeatCalculatorTEM104Service,
+    'ТЭМ-106': CompletionHeatCalculatorTEM106Service,
+}
 
 
-class MainHeatMeterService(Service):
+class MainHeatCalculatorService(Service):
     file = forms.FileField()
     registry = forms.CharField()
 
     def process(self):
+        self.text = PdfReader(self.cleaned_data['file']).pages[
+            0].extract_text().replace('\n', ' ').replace('_', '')
         return self.run_service()
 
     def run_service(self):
-        if self.cleaned_data['registry'] == '28112-04':
-            return CompletionHeatMeter2811204Service.execute(
-                {'registry': self.cleaned_data['registry']},
-                {'file': self.cleaned_data['file']}
-            )
-        elif self.cleaned_data['registry'] == '28112-09':
-            return CompletionHeatMeter2811209Service.execute(
-                {'registry': self.cleaned_data['registry']},
-                {'file': self.cleaned_data['file']}
-            )
-        elif self.cleaned_data['registry'] == '28112-14':
-            return CompletionHeatMeter2811214Service.execute(
-                {'registry': self.cleaned_data['registry']},
-                {'file': self.cleaned_data['file']}
-            )
-        elif self.cleaned_data['registry'] in ['38723-08', '61983-15',
-                                               '61877-15', '25335-13',
-                                               '25335-03', '25335-08']:
-            return CompletionHeatMeter3872308Service.execute(
-                {'registry': self.cleaned_data['registry']},
-                {'file': self.cleaned_data['file']}
-            )
-        elif self.cleaned_data['registry'] in ['45024-10', '71374-18',
-                                               '65782-16', '78403-20']:
-            return CompletionHeatMeter45024410Service.execute(
-                {'registry': self.cleaned_data['registry']},
-                {'file': self.cleaned_data['file']}
-            )
-        elif self.cleaned_data['registry'] == '49318-12':
-            return CompletionHeatMeter4931812Service.execute(
-                {'registry': self.cleaned_data['registry']},
-                {'file': self.cleaned_data['file']}
-            )
-        elif self.cleaned_data['registry'] in ['55440-13', '65137-16']:
-            return CompletionHeatMeter5544013Service.execute(
-                {'registry': self.cleaned_data['registry']},
-                {'file': self.cleaned_data['file']}
-            )
-        elif self.cleaned_data['registry'] == '55457-13':
-            return CompletionHeatMeter5545713Service.execute(
-                {'registry': self.cleaned_data['registry']},
-                {'file': self.cleaned_data['file']}
-            )
-        elif self.cleaned_data['registry'] == '58003-14':
-            return CompletionHeatMeter5800314Service.execute(
-                {'registry': self.cleaned_data['registry']},
-                {'file': self.cleaned_data['file']}
-            )
-        elif self.cleaned_data['registry'] in ['63093-16', '61496-15']:
-            return CompletionHeatMeter6309316Service.execute(
-                {'registry': self.cleaned_data['registry']},
-                {'file': self.cleaned_data['file']}
-            )
-        elif self.cleaned_data['registry'] == '65498-16':
-            return CompletionHeatMeter6549816Service.execute(
-                {'registry': self.cleaned_data['registry']},
-                {'file': self.cleaned_data['file']}
-            )
-        elif self.cleaned_data['registry'] in ['71812-18', '65853-16',
-                                               '63444-16', '66855-17',
-                                               '58595-14']:
-            return CompletionHeatMeter7181218Service.execute(
-                {'registry': self.cleaned_data['registry']},
-                {'file': self.cleaned_data['file']}
-            )
-        else:
-            return CompletionHeatMeterService.execute(
-                {'registry': self.cleaned_data['registry']},
-                {'file': self.cleaned_data['file']}
-            )
+        measuring_instrument_type = self.get_measuring_instrument_type
+        return SERVICES_DICT[measuring_instrument_type].execute(
+            {'registry': self.cleaned_data['registry']},
+            {'file': self.cleaned_data['file']}
+        )
+
+    @property
+    def get_measuring_instrument_type(self):
+        data = collection_data(self.text)
+        return data['measuring_instrument'].split(';')[-2].split(' ')[-1]
--- conf/metrostandart/services/main.py
+++ conf/metrostandart/services/main.py
@@ -6,19 +6,25 @@ from django import forms
 from service_objects.services import Service
 
 from metrostandart.models import Registry
+from metrostandart.services.heat_calculator.main import \
+    MainHeatCalculatorService
 from metrostandart.services.heat_meter.main import MainHeatMeterService
 from metrostandart.services.pressure_gauge.completion_excel import \
     CompletionPressureGaugeExcelService
-from metrostandart.services.pressure_sensor.completion_excel import CompletionPressureSensorExcelService
-from metrostandart.services.thermal_resistance.completion_excel_100p import CompletionThermalResistance100PService
-from metrostandart.services.thermal_resistance.completion_excel_pt100 import CompletionThermalResistancePT100Service
+from metrostandart.services.pressure_sensor.completion_excel import \
+    CompletionPressureSensorExcelService
+from metrostandart.services.thermal_resistance.completion_excel_100p import \
+    CompletionThermalResistance100PService
+from metrostandart.services.thermal_resistance.completion_excel_pt100 import \
+    CompletionThermalResistancePT100Service
 
 
 class MainService(Service):
     file = forms.FileField()
 
     def process(self):
-        self.text = PdfReader(self.cleaned_data['file']).pages[0].extract_text().replace('\n', ' ').replace('_', '')
+        self.text = PdfReader(self.cleaned_data['file']).pages[
+            0].extract_text().replace('\n', ' ').replace('_', '')
         self.validate_file_type()
         self.registry_exists()
         return self.run_service()
@@ -27,17 +33,20 @@ class MainService(Service):
         measuring_instrument = self.registry_exists().measuring_instrument.name
         if measuring_instrument == 'ДАТЧИК ДАВЛЕНИЯ':
             return CompletionPressureSensorExcelService.execute(
-                {'registry': self._get_registry}, {'file': self.cleaned_data['file']}
+                {'registry': self._get_registry},
+                {'file': self.cleaned_data['file']}
             )
         elif measuring_instrument == 'ТЕРМОМЕТР СОПРОТИВЛЕНИЯ':
             type_of_measuring_instrument = self._get_type_thermal_resistance
             if type_of_measuring_instrument == 'Pt100':
                 return CompletionThermalResistancePT100Service.execute(
-                    {'registry': self._get_registry}, {'file': self.cleaned_data['file']}
+                    {'registry': self._get_registry},
+                    {'file': self.cleaned_data['file']}
                 )
             elif type_of_measuring_instrument == '100П':
                 return CompletionThermalResistance100PService.execute(
-                    {'registry': self._get_registry}, {'file': self.cleaned_data['file']}
+                    {'registry': self._get_registry},
+                    {'file': self.cleaned_data['file']}
                 )
         elif measuring_instrument == 'МАНОМЕТРЫ':
             return CompletionPressureGaugeExcelService.execute(
@@ -50,7 +59,7 @@ class MainService(Service):
                 {'file': self.cleaned_data['file']}
             )
         elif measuring_instrument == 'ТЕПЛОВЫЧИСЛИТЕЛЬ':
-            return MainHeatMeterService.execute(
+            return MainHeatCalculatorService.execute(
                 {'registry': self._get_registry},
                 {'file': self.cleaned_data['file']}
             )
@@ -65,7 +74,8 @@ class MainService(Service):
     def _get_type_thermal_resistance(self):
         buff = re.findall(r"(?<=;).*?(?=Рег)", self.text)[0]
         type_thermal = buff.replace(
-            'наименование и обозначение типа, модификация (при наличии) средства измерений, регистрационный номер в', '')
+            'наименование и обозначение типа, модификация (при наличии) средства измерений, регистрационный номер в',
+            '')
         return type_thermal.split(';')[-2].split(' ')[-1][1:-1]
 
     def validate_file_type(self):
@@ -74,7 +84,8 @@ class MainService(Service):
 
     @lru_cache()
     def registry_exists(self):
-        registry = Registry.objects.select_related('measuring_instrument').filter(number=self._get_registry)
+        registry = Registry.objects.select_related(
+            'measuring_instrument').filter(number=self._get_registry)
         if not registry.exists():
             raise Exception('Номер реестра отсутствует в БД')
         return registry.first()
